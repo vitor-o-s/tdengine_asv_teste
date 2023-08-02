8d735e886384531b429a56ec5cb335cce4f762f9
Vitor O <vitorsantos.2018@alunos.utfpr.edu.br>
2023-08-02 00:12:26 -0300

    primeiros benchs

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,18 +1,42 @@
 # Write the benchmarking functions here.
 # See "Writing benchmarks" in the asv docs for more information.
+from ..conexao_tdengine import TDengine 
 
+# , tables, BASE_DIR, ordered_tags_list, get_file_paths, database_name, retention_time, stable_name, schema, tags
+
+database_name = "teste"
+stable_name = "phasor"
+retention_time = "4300d"
+schema = "(ts TIMESTAMP, magnitude FLOAT, Angle FLOAT, frequency FLOAT)"
+tags = "Id BINARY(10)"
+tables = [{"name": "city01", "tag": 1},
+            {"name": "city02", "tag": 2},
+            {"name": "city03", "tag": 3},
+            {"name": "city04", "tag": 4},
+            {"name": "city05", "tag": 5},
+            {"name": "city06", "tag": 6}]
+BASE_DIR = "/home/dell/tcc_package/tdengine_asv_teste/data/"
+ordered_tags_list = ["city01", "city02", "city03", "city04", "city05", "city06"]
 
 class TimeSuite:
     """
     An example benchmark that times the performance of various kinds
     of iterating over dictionaries in Python.
     """
-    def setup(self):
+    """def setup(self):
         self.d = {}
         for x in range(500):
             self.d[x] = None
+    """
+    def setup(self):
+        self.my_object = TDengine(database_name, retention_time,stable_name, schema, tags, tables)
+        self.my_object.create_stable()
+        self.my_object.create_tables()
+    
+    def time_copy_files(self):
+        self.my_object.copy_files(ordered_tags_list, get_file_paths(BASE_DIR, '1klines'))
 
-    def time_keys(self):
+"""    def time_keys(self):
         for key in self.d.keys():
             pass
 
@@ -24,8 +48,10 @@ class TimeSuite:
         d = self.d
         for key in range(500):
             x = d[key]
+"""
 
-
+'''
 class MemSuite:
     def mem_list(self):
         return [0] * 256
+'''
