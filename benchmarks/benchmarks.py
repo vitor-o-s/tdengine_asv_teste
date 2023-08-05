880783c9abfb4f6a86a4c773286b75301a3a8dde
Vitor O <vitorsantos.2018@alunos.utfpr.edu.br>
2023-08-04 21:04:22 -0300

    Passagem para módulo

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,9 +1,11 @@
 # Write the benchmarking functions here.
 # See "Writing benchmarks" in the asv docs for more information.
-
-from conexoes.conexao_tdengine import TDengine
-# from tdengin..conexao_tdengine import TDengine 
-# , tables, BASE_DIR, ordered_tags_list, get_file_paths, database_name, retention_time, stable_name, schema, tags
+import sys
+# print(sys.path)
+sys.path.append(".")
+# sys.path.insert(0, '/home/dell/tcc_package/tdengine_asv_teste/conexoes/')
+# print(sys.path)
+from conexoes.conexao_tdengine import TDengine, tables, BASE_DIR, ordered_tags_list, get_file_paths, database_name, retention_time, stable_name, schema, tags
 
 database_name = "teste"
 stable_name = "phasor"
