565b693ddb5a56f7cbd76321c1391df695b77a8b
Vitor O <vitorsantos.2018@alunos.utfpr.edu.br>
2023-08-05 21:49:07 -0300

    Tentativa importação modulo conexao td

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -5,7 +5,8 @@ import sys
 sys.path.append(".")
 # sys.path.insert(0, '/home/dell/tcc_package/tdengine_asv_teste/conexoes/')
 # print(sys.path)
-from conexoes.conexao_tdengine import TDengine, tables, BASE_DIR, ordered_tags_list, get_file_paths, database_name, retention_time, stable_name, schema, tags
+# from conexoes.conexao_tdengine import TDengine, tables, BASE_DIR, ordered_tags_list, get_file_paths, database_name, retention_time, stable_name, schema, tags
+import conexoes
 
 database_name = "teste"
 stable_name = "phasor"
