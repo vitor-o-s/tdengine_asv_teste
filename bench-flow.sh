9ff9f0ba0b0d1221541bc80b96d060efb63e954c
Vitor O <vitorsantos.2018@alunos.utfpr.edu.br>
2023-09-01 00:48:41 -0300

    Primeira versão do pipeline de teste (sem usar asv)

---
+++ bench-flow.sh
@@ -0,0 +1,57 @@
+echo "Setting Up enviroment"
+source /home/dell/tcc_package/tdengine_asv_teste/.venv/bin/activate
+pip install -r requirements.txt
+
+# Start TDengine
+echo "Starting TD Container"
+sudo docker run -d --name tdengine -p 6030:6030 -p 6041:6041 -p 6043-6049:6043-6049 -p 6043-6049:6043-6049/udp tdengine/tdengine
+wait
+echo "TD is running"
+
+# Run Tests
+echo "Running Tests"
+python3 conexao_tdengine.py
+
+# Stop and remove TDengine
+sudo docker stop tdengine
+sudo docker remove tdengine
+wait
+echo "TDengine Stoped"
+
+# Start Timescale
+echo "Starting Timescale Container"
+sudo sudo docker run -d --name timescaledb -p 127.0.0.1:5432:5432 -e POSTGRES_PASSWORD=password timescale/timescaledb-ha:pg14-latest
+wait
+echo "Timescale is running"
+# Run Tests
+echo "Running Tests"
+python3 conexao_timescale.py
+
+# Stop and remove Timescale
+sudo docker stop timescaledb
+sudo docker remove timescaledb
+wait
+echo "timescaledb Stoped"
+
+# Start Druid
+echo "Downloading files"
+wget 
+wget
+
+echo "Starting Druid Container"
+sudo docker compose up -d
+wait
+echo "Druid is running"
+
+# Run Tests
+echo "Running Tests"
+python3 conexao_druid.py
+
+# Stop and remove Druid
+sudo docker compose down
+# sudo docker remove tdengine
+# wait
+echo "Druid Stoped"
+
+# Generate Results
+python3 gen_graphics.py
