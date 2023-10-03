7525b73328044a517dee93cec845d78c1db4ea12
Vitor O <vitorsantos.2018@alunos.utfpr.edu.br>
2023-10-03 18:43:08 -0300

    Limpeza do código

--- conexoes/conexao_tdengine.py
+++ conexoes/conexao_tdengine.py
@@ -20,7 +20,6 @@ BASE_DIR = "/home/dell/tcc_package/tdengine_asv_teste/data/"
 ordered_tags_list = ["city01", "city02", "city03", "city04", "city05", "city06"]
 
 
-
 class TDengine():
     
     def __init__(
@@ -59,7 +58,6 @@ class TDengine():
         {self.schema} 
         TAGS({self.tags});
         """
-        # print("Running query:\n",create_stable_query)
         self.conn.execute(create_stable_query)
   
     def create_tables(self):
@@ -99,10 +97,7 @@ class TDengine():
         print('ROW_COUNT:', result.row_count)
         rows = result.fetch_all_into_dict()
         print('ROW_COUNT:', result.row_count)
-        return 
-    
-        # for row in rows:
-        #     print('ROW:',row)
+        return
 
     def interval_query(self, table_name: str, start_time: str, end_time: str):
         """
@@ -114,9 +109,6 @@ class TDengine():
         WHERE ts BETWEEN '{start_time}' AND '{end_time}'
         """
         result: taos.TaosResult = self.conn.query(query)
-        # rows = result.fetch_all_into_dict()
-        #for row in rows:
-        #    print('ROW:', row)
 
     def exact_query(self, table_name: str, timestamp: str):
         """
@@ -128,9 +120,6 @@ class TDengine():
         WHERE ts = '{timestamp}'
         """
         result: taos.TaosResult = self.conn.query(query)
-        # rows = result.fetch_all_into_dict()
-        #for row in rows:
-        #    print('ROW:', row)
 
     def aggregation_query(self, table_name: str, aggregation_function: str, column_name: str):
         """
@@ -141,9 +130,6 @@ class TDengine():
         FROM {table_name}
         """
         result: taos.TaosResult = self.conn.query(query)
-        #rows = result.fetch_all_into_dict()
-        #for row in rows:
-        #    print('ROW:', row)
 
     @staticmethod
     def write_to_table(self, conn, table, tags, lines_set):
@@ -196,8 +182,6 @@ if __name__ == "__main__":
    
     my_object = setup()
     # Batch Tests
-    # my_object.copy_files([tables[0]['name']], [BASE_DIR + '1klines/1_loc.csv'])
-    # my_object.copy_files(ordered_tags_list, get_file_paths(BASE_DIR, '1klines'))
     copy_files_lambda2 = lambda: my_object.copy_files(ordered_tags_list, get_file_paths(BASE_DIR, '1klines'))
     print("---COPY time: %s seconds ---" % timeit.timeit(copy_files_lambda2, number=1))
 
@@ -235,65 +219,3 @@ if __name__ == "__main__":
     # Tear Down
     my_object.drop_database()
     my_object.close_conn()
-
-'''
-Timestamp, Austin_V1LPM_Magnitude,Austin_V1LPM_Angle,
-HARRIS_V1LPM_Magnitude,HARRIS_V1LPM_Angle,
-McDonald 1P_V1LPM_Magnitude,McDonald 1P_V1LPM_Angle,
-UT 3 phase_VALPM_Magnitude,UT 3 phase_VALPM_Angle,
-UT Pan Am_V1LPM_Magnitude,UT Pan Am_V1LPM_Angle,
-WACO_V1YPM_Magnitude,WACO_V1YPM_Angle,
-Z_UT_3378_AO[079]_Value,
-Z_UT_3378_AO[085]_Value,
-Z_UT_3378_AO[087]_Value,
-Z_UT_3378_AO[090]_Value,
-Z_UT_3378_AO[091]_Value,
-Z_UT_3378_AO[092]_Value
-
-
-import taos
-
-lines = ["d1001,2018-10-03 14:38:05.000,10.30000,219,0.31000,'California.SanFrancisco',2",
-         "d1004,2018-10-03 14:38:05.000,10.80000,223,0.29000,'California.LosAngeles',3",
-         "d1003,2018-10-03 14:38:05.500,11.80000,221,0.28000,'California.LosAngeles',2",
-         "d1004,2018-10-03 14:38:06.500,11.50000,221,0.35000,'California.LosAngeles',3",
-         "d1002,2018-10-03 14:38:16.650,10.30000,218,0.25000,'California.SanFrancisco',3",
-         "d1001,2018-10-03 14:38:15.000,12.60000,218,0.33000,'California.SanFrancisco',2",
-         "d1001,2018-10-03 14:38:16.800,12.30000,221,0.31000,'California.SanFrancisco',2",
-         "d1003,2018-10-03 14:38:16.600,13.40000,223,0.29000,'California.LosAngeles',2"]
-
-# The generated SQL is:
-# INSERT INTO d1001 USING meters TAGS('California.SanFrancisco', 2) VALUES ('2018-10-03 14:38:05.000', 10.30000, 219, 0.31000) ('2018-10-03 14:38:15.000', 12.60000, 218, 0.33000) ('2018-10-03 14:38:16.800', 12.30000, 221, 0.31000)
-#             d1002 USING meters TAGS('California.SanFrancisco', 3) VALUES ('2018-10-03 14:38:16.650', 10.30000, 218, 0.25000)
-#             d1003 USING meters TAGS('California.LosAngeles', 2) VALUES ('2018-10-03 14:38:05.500', 11.80000, 221, 0.28000) ('2018-10-03 14:38:16.600', 13.40000, 223, 0.29000)
-#             d1004 USING meters TAGS('California.LosAngeles', 3) VALUES ('2018-10-03 14:38:05.000', 10.80000, 223, 0.29000) ('2018-10-03 14:38:06.500', 11.50000, 221, 0.35000)
-
-def get_sql():
-    global lines
-    lines = map(lambda line: line.split(','), lines)  # [['d1001', ...]...]
-    lines = sorted(lines, key=lambda ls: ls[0])  # sort by table name
-    sql = "INSERT INTO "
-    tb_name = None
-    for ps in lines:
-        tmp_tb_name = ps[0]
-        if tb_name != tmp_tb_name:
-            tb_name = tmp_tb_name
-            sql += f"{tb_name} USING meters TAGS({ps[5]}, {ps[6]}) VALUES "
-        sql += f"('{ps[1]}', {ps[2]}, {ps[3]}, {ps[4]}) "
-    return sql
-
-
-def insert_data(conn: taos.TaosConnection):
-    sql = get_sql()
-    affected_rows = conn.execute(sql)
-    print("affected_rows", affected_rows)  # 8
-
-
-if __name__ == '__main__':
-    connection = get_connection()
-    try:
-        create_stable(connection)
-        insert_data(connection)
-    finally:
-        connection.close()
-'''
